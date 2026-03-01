8bcef579ec1abc7eae52dedcc55bf996f03664ef
Zytronium <dastelljes@outlook.com>
2026-03-01 14:07:44 -0600

    Prettify root page slightly

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,7 @@ import Link from "next/link";
 
 export default function Home() {
   return (
-    <>
+    <div className="flex flex-col items-center justify-center min-h-[80vh] w-[80vw] rounded-[80] m-auto bg-dark-blue border-4 border-beige p-4">
       <p>Hello World!</p>
       <div className="h-12"/>
       <span className="text-xl">Links:</span>
@@ -10,6 +10,6 @@ export default function Home() {
         <Link className="button btn-primary" href="/auth">Login/Sign Up</Link>
         <Link className="button btn-primary" href="/ai-example">View Example Chat UI</Link>
       </div>
-    </>
+    </div>
   );
 }
