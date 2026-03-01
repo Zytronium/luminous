ae5a44443247fc3cdc33ec2100d35a41081ca313
Zytronium <dastelljes@outlook.com>
2026-03-01 13:45:02 -0600

    Add buttons to root page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,15 @@
+import Link from "next/link";
+
 export default function Home() {
   return (
-    <p>Hello World!</p>
+    <>
+      <p>Hello World!</p>
+      <div className="h-12"/>
+      <span className="text-xl">Links:</span>
+      <div className="flex gap-4">
+        <Link className="button btn-primary" href="/auth">Login/Sign Up</Link>
+        <Link className="button btn-primary" href="/ai-example">View Example Chat UI</Link>
+      </div>
+    </>
   );
 }
